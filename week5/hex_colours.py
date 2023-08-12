1c8fabac477fcbacaf8813091c883e44b7994d19
shoonleiphyu <shoonleiphyu@my.jcu.edu.au>
2023-08-12 20:34:28 +0630

    codes for colors

--- week5/hex_colours.py
+++ week5/hex_colours.py
@@ -1,6 +1,6 @@
-COLOR_TO_HEX = {"ABSOLUTEZERO": "#0048ba", "ACIDGREEN": "#b0bf1a", "ALICEBLUE": "#f0f8ff", "ALIZARINCRISMON": "#e32636",
-                "AMARANTH": "#e52b50", "AMBER": "#ffbf00", "AMETHYST": "#9966cc", "ANTIQUEWHITE": "#faebd7",
-                "ANTIQUEWHITE1": "#ffefdb", "ANTIQUEWHITE2": "#eedfcc"}
+COLOR_TO_HEX = {"Absolutezero": "#0048ba", "Acidgreen": "#b0bf1a", "Aliceblue": "#f0f8ff", "Alizarincrismon": "#e32636",
+                "Amaranth": "#e52b50", "Amber": "#ffbf00", "Amethyst": "#9966cc", "Antiquewhite": "#faebd7",
+                "Antiquewhite1": "#ffefdb", "Antiquewhite2": "#eedfcc"}
 print(COLOR_TO_HEX)
 
 for color_name, color_code in COLOR_TO_HEX.items():
@@ -10,7 +10,7 @@ color_name = input("Enter a color: ")
 
 while color_name != "":
     try:
-        color_code = COLOR_TO_HEX[color_name.upper()]
+        color_code = COLOR_TO_HEX[color_name.capitalize()]
         print(f"{color_name} is {color_code}")
     except KeyError:
         print("Invalid color name")
