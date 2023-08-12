9337cbe0c11bed42cd9885d8d0c06a3601b28c22
shoonleiphyu <shoonleiphyu@my.jcu.edu.au>
2023-08-12 21:29:41 +0630

    storing emails

---
+++ week5/emails.py
@@ -0,0 +1,22 @@
+def user_name(email):
+    return email.split('@')[0].title()
+
+
+def main():
+    user_data = {}
+    email = input("Email: ")
+    while email != "":
+        name = user_name(email)
+        confirm_name = input(f"Is your name {name}? (Y/n) ").strip().lower()
+
+        if confirm_name == 'n':
+            name = input("Name: ").title()
+
+        user_data[email] = name
+        email = input("Email: ")
+
+    for email, name in user_data.items():
+        print(f"{name} ({email})")
+
+
+main()
