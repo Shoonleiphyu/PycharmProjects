604b501799c8be8e20d935e0449df6be99c8ab63
shoonleiphyu <shoonleiphyu@my.jcu.edu.au>
2023-09-10 17:43:26 +0800

    simple box layout in kv, modified greet and added clear function

--- prac8/box_layout_.py
+++ prac8/box_layout_.py
@@ -9,7 +9,6 @@ class BoxLayoutDemo(App):
         return self.root
 
     def handle_greet(self):
-        # print("Greet")
         print("test")
         self.root.ids.output_label.text = "Hello"
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
@@ -17,5 +16,6 @@ class BoxLayoutDemo(App):
     def handle_clear(self):
         self.root.ids.input_name.text = " "
         self.root.ids.output_label.text = " "
+
 
 BoxLayoutDemo().run()
