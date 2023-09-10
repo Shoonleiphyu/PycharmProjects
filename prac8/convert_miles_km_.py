503da372a137f773eddc04cdf6e5333608bb4c6a
shoonleiphyu <shoonleiphyu@my.jcu.edu.au>
2023-09-10 17:27:59 +0800

    modified output label to str

---
+++ prac8/convert_miles_km_.py
@@ -0,0 +1,37 @@
+
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+MILES_TO_KM = 1.60934
+
+
+class MilesConverterApp(App):
+    output_label = StringProperty('')
+
+    def build(self):
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file('convert_miles_km_.kv')
+        #self.output_label = "Kilometer will appear here"
+        return self.root
+
+    def handle_calculate(self):
+        value = self.get_validated_miles()
+        result = value * MILES_TO_KM
+        self.output_label = str(result)
+
+    def handle_increment(self, change):
+        value = self.get_validated_miles() + change
+        self.root.ids.input_miles.text = str(value)
+        self.handle_calculate()
+
+    def get_validated_miles(self):
+        try:
+            value = float(self.root.ids.input_miles.text)
+            return value
+        except ValueError:
+            return 0
+
+
+MilesConverterApp().run()
