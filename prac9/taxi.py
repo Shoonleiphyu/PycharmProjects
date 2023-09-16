b9352b1b6af072cec634a6c0b40519a2d570d0ac
shoonleiphyu <shoonleiphyu@my.jcu.edu.au>
2023-09-16 22:37:09 +0800

    modified taxi class with new variables

--- prac9/taxi.py
+++ prac9/taxi.py
@@ -9,7 +9,7 @@ class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
     price_per_km = 1.23
 
-    def __init__(self, name, fuel,):
+    def __init__(self, name, fuel):
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
 
@@ -18,9 +18,7 @@ class Taxi(Car):
     def __str__(self):
         """Return a string like a Car but with current fare distance."""
         return f"{super().__str__()}, {self.current_fare_distance}km on current fare, ${self.price_per_km:.2f}/km"
-        #return "{}, {}km on current fare, ${:.2f}/km".format(super().__str__(),
-        #                                                     self.current_fare_distance,
-        #                                                     self.price_per_km)
+
 
     def get_fare(self):
         """Return the price for the taxi trip."""
